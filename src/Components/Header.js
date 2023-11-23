3aeae3d756fa48a4d7bc7f0ea66c545861c97fe2
NGSB <k971711@naver.com>
2023-11-23 22:39:37 +0900

    commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,16 +2,38 @@ import React from "react";
 import '../Style/Header.css'
 
 function Header(){
+
+    const scroll = (position) => {
+        window.scrollTo({
+            top: position,
+            left: 0,
+            // behavior: 'smooth',
+        })
+    }
+
+    const goHome = () => {
+        scroll(0)
+    }
+    const first = () => {
+        scroll(window.innerHeight)
+    }
+    const second = () => {
+        scroll(window.innerHeight * 2)
+    }
+    const third = () => {
+        scroll(window.innerHeight * 3)
+    }
+
     return(
         <div className="Header_container">
             <div className="header_name">
                 <p>NGSB</p>
             </div>
             <div className="header_index">
-                <p>홈</p>
-                <p>목차</p>
-                <p>목차</p>
-                <p>목차</p>
+                <p className="home" onClick={goHome}>홈</p>
+                <p className="first" onClick={first}>목차</p>
+                <p className="second" onClick={second}>목차</p>
+                <p className="third" onClick={third}>목차</p>
             </div>
         </div>
     )
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,29 +2,28 @@ import React from "react";
 import '../Style/Profile.css'
 
 import { MdLocalPhone } from "react-icons/md";
-import { AiOutlineGithub, AiOutlineMail, AiOutlineUser } from "react-icons/ai";
+import { AiOutlineMail, AiOutlineUser } from "react-icons/ai";
 
 function Profile(){
     return(
         <div className="Profile_container">
+            {/* <p> 깔끔하고 단단한 프론트엔드 개발자입니다. </p> */}
             <div className="name_card">
                 <div className="name_card_img">
-                    
+                    사진
                 </div>
-                <div className="name_card_text">
+                <div className="name_card_area">
                     <p className="name_card_text_title">Front-end Engineer</p>
-                    <div className="name_card_text_info">
-                        <div>
-                            <AiOutlineUser />
-                            <MdLocalPhone />
-                            <AiOutlineMail />
-                            <AiOutlineGithub />
+                    <div className="name_card_info">
+                        <div className="name_card_info_icon">
+                            <span><AiOutlineUser /></span>
+                            <span><MdLocalPhone /></span>
+                            <span><AiOutlineMail /></span>
                         </div>
-                        <div>
-                            <p>남궁성빈</p>
-                            <p>010-5310-9717</p>
-                            <p>k971711@naver.com</p>
-                            <p>GitHub</p>
+                        <div className="name_card_info_text">
+                            <span>남궁성빈</span>
+                            <span>010-5310-9717</span>
+                            <span>k971711@naver.com</span>
                         </div>
                     </div>
                 </div>
