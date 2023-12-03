e2bd549c915c195b99ab1a5f0e9fe199c77d0a91
NGSB <k971711@naver.com>
2023-12-03 23:19:58 +0900

    commit

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -8,38 +8,40 @@ import { GrLocation } from "react-icons/gr";
 function Profile(){
     return(
         <div className="Profile_container">
-            <span className="self_confidence"> 한결같은 프론트엔드 개발자입니다. </span>
-            <div className="name_card">
+            <div className="self_introduction">
                 <div className="name_card_img">
                     사진
                 </div>
+                <div className="self_confidence">
+                    <span>한결같은 프론트엔드 개발자가 되고싶은 남궁성빈입니다.</span>
+                    <span>우송대학교 컴퓨터 정보·보안을 졸업하고</span>
+                </div>
+                <a href="https://github.com/SeongBinBin" target="_blank" rel="noopener noreferrer" className="name_card_info_icon"><AiFillGithub size={25}/></a>
+            </div>
+            <div className="name_card">
                 <div className="name_card_area">
-                    <p className="name_card_text_title">Front-end Engineer</p>
-                    <div className="name_card_info">
-                        <div className="name_card_info_box">
-                            <span className="name_card_info_icon"><AiOutlineUser size={20}/></span>
+                    {/* <div className="name_card_info"> */}
+                        {/* <div className="name_card_info_box">
+                            <span className="name_card_info_icon"><AiOutlineUser size={25}/></span>
                             <span className="name_card_info_text">남궁성빈</span>
-                        </div>
+                        </div> */}
                         <div className="name_card_info_box">
-                            <span className="name_card_info_icon"><MdLocalPhone size={20}/></span>
+                            <span className="name_card_info_icon"><MdLocalPhone size={25}/></span>
                             <span className="name_card_info_text">010-5310-9717</span>
                         </div>
                         <div className="name_card_info_box">
-                            <span className="name_card_info_icon"><AiOutlineMail size={20}/></span>
+                            <span className="name_card_info_icon"><AiOutlineMail size={25}/></span>
                             <span className="name_card_info_text">k971711@naver.com</span>
                         </div>
                         <div className="name_card_info_box">
-                            <span className="name_card_info_icon"><GrLocation size={20}/></span>
+                            <span className="name_card_info_icon"><GrLocation size={25}/></span>
                             <span className="name_card_info_text">대전광역시 대덕구</span>
                         </div>
                         <div className="name_card_info_box">
-                            <span className="name_card_info_icon"><MdOutlineDateRange size={20}/></span>
+                            <span className="name_card_info_icon"><MdOutlineDateRange size={25}/></span>
                             <span className="name_card_info_text">1997. 07. 28</span>
                         </div>
-                        <div className="name_card_info_box">
-                            <a href="https://github.com/SeongBinBin" target="_blank" rel="noopener noreferrer" className="name_card_info_icon"><AiFillGithub size={25}/></a>
-                        </div>
-                    </div>
+                    {/* </div> */}
                 </div>
             </div>
         </div>
